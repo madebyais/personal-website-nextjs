ebaa8d9712241413d602f4fabee9ea3e0e75e2fd
Faris <madebyais@gmail.com>
2021-07-17 18:44:27 +0700

    Revert "Remove css tags from _document and add fontawesome 4 into style.less"
    This reverts commit 43a5d645d4141c08604ddb80e62591815fa455db.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,7 @@ class _Document extends Document {
       <Html>
         <Head>
           <link rel='icon' href='/favicon.ico' />
+          <link rel='stylesheet' href='//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css' integrity='sha512-SfTiTlX6kk+qitfevl/7LibUOeJWlt9rbyDn92a1DqWOw9vWG2MFoays0sgObmWazO5BQPiFucnnEAjpAB+/Sw==' crossOrigin='anonymous' />
         </Head>
         <body>
           <Main />
